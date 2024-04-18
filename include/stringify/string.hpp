81642b080456b31a386b9c467ab776f0098030e7
Stronkkey <Ail45000@proton.me>
2024-04-18 18:14:53 +0200

    Fix StringView type < cpp 14 name

--- include/stringify/string.hpp
+++ include/stringify/string.hpp
@@ -6,11 +6,13 @@
 #define STRINGIFY_STRING_TYPE ::std::string
 #endif
 
+
 #ifndef STRINGIFY_STRING_VIEW_TYPE
 #if __cplusplus >= 201703L
+#include <string_view>
 #define STRINGIFY_STRING_VIEW_TYPE ::std::basic_string_view<STRINGIFY_STRING_TYPE::value_type>
 #else
-#define STRSTRINGIFY_STRING_VIEW_TYPE STRINGIFY_STRING_TYPE
+#define STRINGIFY_STRING_VIEW_TYPE STRINGIFY_STRING_TYPE
 #endif
 #endif
 
